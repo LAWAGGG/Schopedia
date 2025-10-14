54bd91d9e16befaac117f2a037ae1ccc6fbbbdf7
Erzy <tchhehe@gmail.com>
2025-10-14 13:16:44 +0700

    update

--- SP-frontend/src/pages/dashboard/dashboard.jsx
+++ SP-frontend/src/pages/dashboard/dashboard.jsx
@@ -3,7 +3,7 @@ import "./../../styles/dashboard.css";
 export default function Dashboard() {
     return(
         <div className="dashboard">
-            <h1>IRMAAAAAA</h1>
+            <h1>LOVE U</h1>
         </div>
     )
 }
