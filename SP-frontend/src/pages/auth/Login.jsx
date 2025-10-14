ece6e61f75b32865f2441249231386cea02f4724
Erzy <tchhehe@gmail.com>
2025-10-14 13:07:49 +0700

    perbaiki

--- SP-frontend/src/pages/auth/Login.jsx
+++ SP-frontend/src/pages/auth/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
 
     async function HandleLogin(e) {
         e.preventDefault();
-        const res = await fetch("http://localhost:8000/api/login", {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}api/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
