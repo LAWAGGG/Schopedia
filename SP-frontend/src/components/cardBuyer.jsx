26d956edff341a1279e9b62e0036dedb874da1c8
Erzy <tchhehe@gmail.com>
2025-10-21 12:56:34 +0700

    harga

--- SP-frontend/src/components/cardBuyer.jsx
+++ SP-frontend/src/components/cardBuyer.jsx
@@ -1,4 +1,4 @@
-export default function CardBuyer({ product, hideButton}) {
+export default function CardBuyer({ product, hideButton }) {
   return (
     <div className="bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-purple-400 hover:bg-purple-100 transition duration-300 min-w-[220px] flex-shrink-0 cursor-pointer">
       <div className="w-full h-48 overflow-hidden flex justify-center items-center bg-gray-50">
@@ -12,7 +12,7 @@ export default function CardBuyer({ product, hideButton}) {
       <div className="p-4 text-center">
         <h2 className="text-lg font-semibold text-gray-800">{product.name}</h2>
         <p className="text-gray-600 mb-4">
-          Rp {Number(product.price || 0).toLocaleString("id-ID")}
+          {product.price || "Rp 0"}
         </p>
       </div>
     </div>
