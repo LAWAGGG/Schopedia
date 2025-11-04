102fc74b48920414bf5ad17fa83b97da3361c113
LAWAGGG <ahmadfagih.arrifai@gmail.com>
2025-11-04 08:40:47 +0700

    connect api to chart

--- SP-frontend/src/components/charts.jsx
+++ SP-frontend/src/components/charts.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
     LineChart,
     Line,
@@ -9,30 +9,56 @@ import {
     Legend,
     ResponsiveContainer,
 } from "recharts";
-
-// Data penjualan harian Senin - Minggu
-const dailySalesData = [
-    { Month: "januari", orders : 500 },
-    { Month: "februari", orders : 700 },
-    { Month: "maret", orders : 600 },
-    { Month: "april", orders : 800 },
-    { Month: "mei", orders : 750 },
-    { Month: "juni", orders : 900 },
-    { Month: "agustus", orders : 650 },
-    { Month: "september", orders : 400 },
-    { Month: "oktober", orders : 200 },
-    { Month: "november", orders : 700 },
-    { Month: "desember", orders : 100 },
-];
+import { getToken } from "../utils/utils";
 
 export default function Charts() {
+    const [chartData, setChartData] = useState([]);
+
+    const fetchMonthlySales = async () => {
+        try {
+            const response = await fetch("http://localhost:8000/api/selling/orders", {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": `Bearer ${getToken()}`,
+                },
+            });
+
+            const data = await response.json();
+
+            if (data.orders) {
+
+                const monthOrder = [
+                    "januari", "februari", "maret", "april", "mei", "juni",
+                    "juli", "agustus", "september", "oktober", "november", "desember"
+                ];
+
+                const formattedData = Object.keys(data.orders)
+                    .sort((a, b) => monthOrder.indexOf(a) - monthOrder.indexOf(b))
+                    .map(month => ({
+                        Month: month,
+                        orders: data.orders[month],
+                    }));
+
+                setChartData(formattedData);
+            }
+
+        } catch (error) {
+            console.error("Error fetching monthly sales:", error);
+        }
+    };
+
+
+    useEffect(() => {
+        fetchMonthlySales();
+    }, []);
+
     return (
         <div className="p-0">
-            {/* Grafik Penjualan Harian */}
             <div className="bg-white p-6 rounded-lg shadow-xl">
                 <h2 className="text-xl font-bold mb-4">Grafik Orderan Bulanan</h2>
                 <ResponsiveContainer width="100%" height={300}>
-                    <LineChart data={dailySalesData} margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
+                    <LineChart data={chartData} margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="Month" />
                         <YAxis />
